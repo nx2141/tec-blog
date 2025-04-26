65b89279157733e2d4a6699d788ff8fc64264542
Makoto <132969568+nx2141@users.noreply.github.com>
2025-04-26 18:08:16 +0900

    add:prose うまくいかないが

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+const typography = require("@tailwindcss/typography");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
